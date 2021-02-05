9d87d2f7ea7ff138a2083d6c9f0c5c2696a01cbd
Shota <s.ito@wiz-net.jp>
2021-02-05 11:28:01 +0900

    途中経過

---
+++ BowlingScore.rb
@@ -0,0 +1,34 @@
+class BowlingScore
+    @point
+    @score
+    @count
+  
+    def initialize(point)
+      @point = point
+      @score = []
+      @count = []
+    end
+  
+    def point()
+      return @point
+    end
+  
+    def add_score(shot)
+      @count << shot
+      if shot.size == 2
+          @score << acount
+      end
+  
+    def getPoint()
+        @point.each.with_index do |frame, i|
+      if  
+  # 初回だけストライク10点 + 次のフレームの1投目 + 次のフレームの2投目
+  #   else
+      end
+  
+    end
+  
+  scores = []
+  
+  scores[0] = BowlingScore.new()
+  
